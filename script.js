9bdf4cc368e874ace85f8e8d09d3372f5977a02c
rupam404 <108481330+rupam404@users.noreply.github.com>
2022-08-26 16:01:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let calculate = document.querySelector('#calculate');
+
+calculate.addEventListener('click', () => {
+    let totalBill = document.getElementById("totalBill").value;
+    let tipPercent = document.getElementById("tipPercent").value;
+    let split = document.getElementById("split").value;
+
+    
+    if(totalBill === '' || tipPercent === 0 || split === 0){
+        alert('Please enter values');
+        return;
+    }
+    
+    let total = (totalBill * tipPercent) / split;
+    total = total.toFixed(2);
+    
+    document.getElementById("tip").innerHTML = total;
+
+
+
+})
